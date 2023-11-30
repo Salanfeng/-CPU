b95b40c828091190673ba0c00c60da9be17b97c9
飒岚枫 <2309411165@qq.com>
2023-11-30 11:02:44 +0800

    tmp

--- mdu.v
+++ mdu.v
@@ -34,19 +34,19 @@ always @(*) begin
         case (MDUOp)
             4'b0001: begin
                 {HI_t, LO_t} = $signed(A) * $signed(B);
-                BusyTime = 32'd4;
+                BusyTime = 32'd5;
             end
             4'b0010: begin
                 {HI_t, LO_t} = A * B;
-                BusyTime = 32'd4;
+                BusyTime = 32'd5;
             end
             4'b0011: begin
                 {HI_t, LO_t} = B == 0 ? {HI_t, LO_t} : {$signed(A) % $signed(B), $signed(A) / $signed(B)};
-                BusyTime = 32'd9;
+                BusyTime = 32'd10;
             end
             4'b0100: begin
                 {HI_t, LO_t} = B == 0 ? {HI_t, LO_t} : {$unsigned(A) % $unsigned(B), $unsigned(A) / $unsigned(B)};
-                BusyTime = 32'd9;
+                BusyTime = 32'd10;
             end
             4'b0101: begin
                 Out_t = HI_t;
